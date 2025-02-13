b0c74e69d3bb9f7aedd9a6eaaf275b6ff9b3cf0e
dinkol <102145254+dinkol@users.noreply.github.com>
2025-02-13 14:55:39 +0100

    Update main.tf

--- 01_Foundation/main.tf
+++ 01_Foundation/main.tf
@@ -13,10 +13,6 @@ terraform {
   }
   backend "azurerm" {
     key      = "terraform.tfstate"
-    client_id            = "${{ secrets.AZURE_CLIENT_ID }}"  # Can also be set via `ARM_CLIENT_ID` environment variable.
-    subscription_id      = "${{ secrets.AZURE_SUBSCRIPTION_ID }}"  # Can also be set via `ARM_SUBSCRIPTION_ID` environment variable.
-    tenant_id            = "${{ secrets.AZURE_TENANT_ID }}"  # Can also be set via `ARM_TENANT_ID` environment variable.
-
     use_oidc = true
   }
 }
