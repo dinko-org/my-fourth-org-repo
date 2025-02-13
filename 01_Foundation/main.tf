9e0d5a8af59fadd01707d403ecd50edcd0e4af59
dinkol <102145254+dinkol@users.noreply.github.com>
2025-02-13 14:06:26 +0100

    Create main.tf

---
+++ 01_Foundation/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">=1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.30.0"
+    }
+  }
+  backend "azurerm" {
+    key      = "terraform.tfstate"
+    use_oidc = true
+  }
+}
+provider "azurerm" {
+  features {
+  features {
+    key_vault {
+      recover_soft_deleted_key_vaults = true
+      purge_soft_delete_on_destroy    = true
+    }
+  }
+  }
+  use_oidc = true
+}
